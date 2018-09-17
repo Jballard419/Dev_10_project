788854627d8027e5747d61a3f097733749abe6a6
James Ballard <j.lb4@live.com>
2018-09-17 14:40:45 -0500

    better documented

--- Restaurant1/contact.js
+++ Restaurant1/contact.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded',function(){
 
   // document.querySelector('select[name="contact"]').onchange=checkSelectHandler;
-  document.querySelector('form[name="myForm"]').onchange=checkNameHandler;
+  document.querySelector('form[name="myForm"]').onchange=checkNameHandler; // onchange to wait until user is done with text based inputs
+
+  //add format error messages 
   document.querySelector('input[name="email"]').onchange=checkFormatHandler;
   document.querySelector('input[name="phone"]').onchange=checkFormatHandler;
 
@@ -14,11 +16,19 @@ document.addEventListener('DOMContentLoaded',function(){
 
 function checkNameHandler(event) {
   if(document.forms["myForm"].checkValidity()){
-     if($("input:checkbox:checked").length!=0) setTimeout(()=>{alert("Form info complete")},0);
+     if($("input:checkbox:checked").length!=0) {
+       setTimeout(()=>{alert("Form info complete")},0);
+       //Set timeout so message appears after a checkmark/radio is selected
+     };
     // event.target.setAttribute("style", "border:2px  darkred solid");
   }
 
 }
+
+/* name:checkFormatHandler(event)
+* purpose: display message if user input incorrect format
+
+*/
 function checkFormatHandler(event) {
   if(!event.target.checkValidity()){
     event.target.classList.add("is-invalid");
